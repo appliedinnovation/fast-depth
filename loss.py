ce5ccb072ef60262b548389bc4c49f01f1e57198
Alex Barnett <agraybarnett@gmail.com>
2021-01-07 13:58:20 -0600

    added berhu loss (untested)

--- loss.py
+++ loss.py
@@ -7,7 +7,8 @@ def get_loss(loss_str):
     loss_dict = {
         "l1": torch.nn.L1Loss(),
         "l2": torch.nn.MSELoss(),
-        "silog": SILogLoss()
+        "silog": SILogLoss(),
+        "berhu": BerhuLoss()
     }
 
     return loss_dict[loss_str]
@@ -30,3 +31,31 @@ class SILogLoss(nn.Module):
 
         Dg = torch.var(g) + 0.15 * torch.pow(torch.mean(g), 2)
         return 10 * torch.sqrt(Dg)
+
+class BerhuLoss(nn.Module):
+    def __init__(self):
+        super(BerhuLoss, self).__init__()
+        self.name = 'Berhu'
+
+    def forward(self, input, target, mask=None):
+        if mask is not None:
+            input = input[mask]
+            target = target[mask]
+        
+        # C threshold
+        c = 0.2 * torch.max(torch.abs(target - input))
+
+        sum_loss = 0.0
+        for pred, depth in zip(input, target):
+            for pred_row, depth_row in zip(pred[0], depth[0]):
+                for pred_pixel, depth_pixel in zip(pred_row, depth_row):
+                    x = torch.abs(pred_pixel - depth_pixel)
+                    if x <= c:
+                        sum_loss += x
+                    else:
+                        sum_loss += (1 / (2 * c)) * (torch.square(x) + torch.square(c))
+        
+        # Calculate the mean
+        loss = sum_loss / input.shape[0]
+
+        return loss
