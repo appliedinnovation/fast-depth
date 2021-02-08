3c54a3318a8e40385a9f3c43bdb97c793cd24713
Alex Barnett <agraybarnett@gmail.com>
2021-02-08 15:13:58 -0600

    WIP updating evaluate script

--- evaluate.py
+++ evaluate.py
@@ -1,3 +1,4 @@
+import Datasets
 from comet_ml import Experiment, ExistingExperiment
 import utils
 from metrics import AverageMeter, Result
@@ -20,21 +21,24 @@ try:
 except KeyError:
     print("Datasets.py absolute path not found in PATH")
 sys.path.append(dataset_path)
-import Datasets
 
 fieldnames = ['rmse', 'mae', 'delta1', 'absrel',
-              'lg10', 'mse', 'delta2', 'delta3', 'data_time', 'gpu_time']
+              'lg10', 'mse', 'delta2', 'delta3', 'irmse', 'imae', 'data_time', 'gpu_time']
+
 
 def main(args):
 
     print("Loading config file: ", args.config)
     params = utils.load_config_file(args.config)
-    params["test_dataset_paths"] = utils.format_dataset_path(params["test_dataset_paths"])
-    
+    params["test_dataset_paths"] = utils.format_dataset_path(
+        params["test_dataset_paths"])
+
     if "experiment_key" in params:
-        experiment = ExistingExperiment(api_key="jBFVYFo9VUsy0kb0lioKXfTmM", previous_experiment=params["experiment_key"])
+        experiment = ExistingExperiment(
+            api_key="jBFVYFo9VUsy0kb0lioKXfTmM", previous_experiment=params["experiment_key"])
     else:
-        experiment = Experiment(api_key="jBFVYFo9VUsy0kb0lioKXfTmM", project_name="fastdepth")
+        experiment = Experiment(
+            api_key="jBFVYFo9VUsy0kb0lioKXfTmM", project_name="fastdepth")
 
     # Data loading code
     print("Creating data loaders...")
@@ -44,8 +48,8 @@ def main(args):
     else:
         val_dataset = Datasets.FastDepthDataset(params["test_dataset_paths"],
                                                 split='val',
-                                                depth_min = params["depth_min"],
-                                                depth_max = params["depth_max"],
+                                                depth_min=params["depth_min"],
+                                                depth_max=params["depth_max"],
                                                 input_shape_model=(224, 224),
                                                 disparity=params["predict_disparity"],
                                                 random_crop=False
@@ -64,13 +68,14 @@ def main(args):
     else:
         # Maintain compatibility for fastdepth NYU model format
         state_dict = torch.load(args.model, map_location=params["device"])
-        model = models.MobileNetSkipAdd(output_size=(224, 224), pretrained=True)
+        model = models.MobileNetSkipAdd(
+            output_size=(224, 224), pretrained=True)
         model.load_state_dict(state_dict)
         params["start_epoch"] = 0
 
     # Set GPU
     params["device"] = torch.device("cuda:{}".format(params["device"])
-                          if params["device"] >= 0 and torch.cuda.is_available() else "cpu")
+                                    if params["device"] >= 0 and torch.cuda.is_available() else "cpu")
     print("Using device", params["device"])
 
     model.to(params["device"])
@@ -90,7 +95,8 @@ def evaluate(params, loader, model, experiment):
     with experiment.test():
         with torch.no_grad():
             average = AverageMeter()
-            img_idxs = np.random.randint(0, len(loader), size=min(len(loader), 50))
+            img_idxs = np.random.randint(
+                0, len(loader), size=min(len(loader), 50))
             end = time.time()
             for i, (inputs, targets) in enumerate(loader):
                 inputs, targets = inputs.to(
@@ -102,7 +108,7 @@ def evaluate(params, loader, model, experiment):
                 end = time.time()
                 outputs = model(inputs)
                 gpu_time = time.time() - end
-                
+
                 # Flip to depth if necessary
                 outputs, targets = utils.convert_to_depth(
                     outputs, targets, not_clipped_yet=True, is_disparity=params["predict_disparity"], clip=1.0 / params["depth_max"])
@@ -114,12 +120,14 @@ def evaluate(params, loader, model, experiment):
 
                 # Log images to comet
                 if i in img_idxs:
-                    img_merge = utils.merge_into_row(inputs[0], targets[0], outputs[0])
+                    img_merge = utils.merge_into_row(
+                        inputs[0], targets[0], outputs[0])
                     img_merge = utils.write_results(img_merge, result)
-                    utils.log_merged_image_to_comet(img_merge, 0, i, experiment, "test")
-                    if params["save_test_images"]:                    
-                        filename = os.path.join(params["experiment_dir"], \
-                            "comparison_epoch_{}_{}.png".format(str(params["start_epoch"]), np.where(img_idxs == i)[0][0]))
+                    utils.log_merged_image_to_comet(
+                        img_merge, 0, i, experiment, "test")
+                    if params["save_test_images"]:
+                        filename = os.path.join(params["experiment_dir"],
+                                                "comparison_epoch_{}_{}.png".format(str(params["start_epoch"]), np.where(img_idxs == i)[0][0]))
                         utils.save_image(img_merge, filename)
 
                 if (i + 1) % params["stats_frequency"] == 0 and i != 0:
@@ -140,23 +148,27 @@ def evaluate(params, loader, model, experiment):
                   'MAE={average.mae:.3f}\n'
                   'Delta1={average.delta1:.3f}\n'
                   'REL={average.absrel:.3f}\n'
-                  'Lg10={average.lg10:.3f}\n'    
+                  'Lg10={average.lg10:.3f}\n'
                   't_GPU={time:.3f}\n'.format(average=avg, time=avg.gpu_time))
 
             if params["save_test_metrics"]:
-                filename = os.path.join(params["experiment_dir"], "results.csv")
+                filename = os.path.join(
+                    params["experiment_dir"], "results.csv")
                 with open(filename, 'a') as csvfile:
                     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
                     writer.writerow({'mse': avg.mse, 'rmse': avg.rmse, 'absrel': avg.absrel, 'lg10': avg.lg10,
-                                    'mae': avg.mae, 'delta1': avg.delta1, 'delta2': avg.delta2, 'delta3': avg.delta3,
-                                    'data_time': avg.data_time, 'gpu_time': avg.gpu_time})
+                                     'mae': avg.mae, 'delta1': avg.delta1, 'delta2': avg.delta2, 'delta3': avg.delta3, 'irmse': avg.irmse, 'imae': avg.imae,
+                                     'data_time': avg.data_time, 'gpu_time': avg.gpu_time})
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='FastDepth evaluation')
-    parser.add_argument('-m', '--model', type=str, required=True, help="Model path.")
-    parser.add_argument('-c', '--config', type=str, default="evaluate_config.json", help="Path to config JSON.")
-    parser.add_argument('-e', '--existing-experiment', type=str, help="Comet Existing Experiment key")
+    parser.add_argument('-m', '--model', type=str,
+                        required=True, help="Model path.")
+    parser.add_argument('-c', '--config', type=str,
+                        default="evaluate_config.json", help="Path to config JSON.")
+    parser.add_argument('-e', '--existing-experiment',
+                        type=str, help="Comet Existing Experiment key")
     parser.add_argument('--nyu', type=int, default=0,
                         help='whether to use NYU Depth V2 dataset.')
     args = parser.parse_args()
